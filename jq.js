48ac41a6a23746ce678e0a16a70754f94eebc21a
Lukasz Wieczorek <wieczorek.web@gmail.com>
2019-05-19 21:18:06 +0200

    jq

--- jq.js
+++ jq.js
@@ -6,52 +6,34 @@ $(window).on('scroll', function(){
         $("nav").removeClass("black");
     }
 })
-// slide to sections
-$(".about-nav").on("click", () => {
-    $("html,body").animate({
-        scrollTop: $(".about-us").offset().top - $("nav").innerHeight()
-    },500);
-});
+// buton slide to section about
+ $(".button").on("click", () => {
+     $("html,body").animate({
+        scrollTop: $(".about-us").offset().top - $("nav").outerHeight()
+     },500);
+ });
+// logo slide back to top of the site
+ $(".logo").on("click", () => {
+     $("html,body").animate({
+         scrollTop: $("body").offset().top -  $("nav").outerHeight()
+     },500);
+ });
+// nav slide to section
+$("nav a").on("click", function(){
+    const goToSection = "#"+ $(this).attr("class");
+    $("body,html").animate({
+        scrollTop:$(goToSection).offset().top - $("nav").outerHeight()
+    },500)
 
-$(".offer-nav").on("click", () => {
-    $("html,body").animate({
-        scrollTop: $(".offer").offset().top - $("nav").innerHeight()
-    },500);
-});
-
-$(".gallery-nav").on("click", () => {
-    $("html,body").animate({
-        scrollTop: $(".gallery").offset().top - $("nav").innerHeight()
-    },500);
-});
-
-$(".contact-nav").on("click", () => {
-    $("html,body").animate({
-        scrollTop: $(".contact").offset().top - $("nav").innerHeight()
-    },500);
-});
-
-$(".question-nav").on("click", () => {
-    $("html,body").animate({
-        scrollTop: $(".question").offset().top - $("nav").innerHeight()*2.5
-    },500);
-});
-
-$(".button").on("click", () => {
-    $("html,body").animate({
-        scrollTop: $(".about-us").offset().top - $("nav").innerHeight()
-    },500);
-});
-
-$(".logo").on("click", () => {
-    $("html,body").animate({
-        scrollTop: $("body").offset().top - $("nav").innerHeight()
-    },500);
-});
+    
+})
 
 // hiding menu-mobile
 $("ul li").on("click", () => {
 $("nav ul").toggleClass("active");
 $(".menu-mobile").toggleClass("active");
 
+
 }) 
+
+ 
